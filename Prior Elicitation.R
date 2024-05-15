090551a6594c614efa32a331d2f86f9bb7ad0bdd
GiovanniRebaudo <rebaudo.giovanni@gmail.com>
2024-05-15 16:27:30 +0200

    Update Prior Elicitation.R

--- Prior Elicitation.R
+++ Prior Elicitation.R
@@ -1,7 +1,8 @@
 # Prior elicitation
+nSamples = 2000
 # HDP
-alpha0Samples = rgamma(2000, shape=1, rate=1/3)
-alpha1Samples = rgamma(2000, shape=1, rate=1/2)
+alpha0Samples = rgamma(nSamples, shape=1, rate=1/3)
+alpha1Samples = rgamma(nSamples, shape=1, rate=1/2)
 
 probTiesWithinSamples = 
   (1+alpha0Samples+ alpha1Samples)/((1+alpha0Samples)*(1+alpha1Samples))
@@ -10,10 +11,13 @@ hist(probTiesWithinSamples)
 mean(probTiesWithinSamples);var(probTiesWithinSamples)
 
 # DP
-alphaSamples = rgamma(2000, shape=1, rate=1)
+alphaSamples = rgamma(nSamples, shape=1, rate=1)
 probTiesWithinSamples = 1/(alphaSamples+1)
 hist(probTiesWithinSamples)
 mean(probTiesWithinSamples);var(probTiesWithinSamples)
 
 # HPYP
 
+((1 - sigma0Samples * sigma1Samples)+
+    alpha1Samples *(1-sigma1Samples) + alpha0Samples*(1-sigma0Samples ))/
+  ((1+alpha1Samples)*(1+alpha0Samples)) 
